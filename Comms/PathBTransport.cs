6e0e044bdb8ccf23b5934d4e9ebc4c929b21e79d
artriy <artriy@users.noreply.github.com>
2026-06-26 08:21:06 +0300

    feat(pathb): add SidecarVoiceTransport and RpcSignalingSender adapters

---
+++ Comms/PathBTransport.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Globalization;
+
+namespace VoiceChatPlugin.VoiceChat;
+
+internal sealed class RpcSignalingSender : ISignalingSender
+{
+    public void Send(int targetClientId, SignalMsgType type, byte[] payload)
+        => AmongUsRpcSignaling.Send(targetClientId, type, payload);
+}
+
+#if WINDOWS
+internal sealed class SidecarVoiceTransport : IVoiceTransport
+{
+    private readonly Func<SidecarVoiceClient?> _voice;
+
+    public SidecarVoiceTransport(Func<SidecarVoiceClient?> voice)
+    {
+        _voice = voice ?? throw new ArgumentNullException(nameof(voice));
+    }
+
+    public static string PeerId(int clientId) => clientId.ToString(CultureInfo.InvariantCulture);
+
+    public static bool TryParseClientId(string peerId, out int clientId)
+        => int.TryParse(peerId, NumberStyles.Integer, CultureInfo.InvariantCulture, out clientId);
+
+    public void AddPeer(int clientId) => _voice()?.AddPeer(PeerId(clientId));
+
+    public void RemovePeer(int clientId) => _voice()?.RemovePeer(PeerId(clientId));
+
+    public void SetRemoteSdp(int clientId, string sdpType, string sdp) => _voice()?.SetRemoteSdp(PeerId(clientId), sdpType, sdp);
+
+    public void AddIceCandidate(int clientId, string candidate) => _voice()?.AddIceCandidate(PeerId(clientId), candidate);
+}
+#endif
